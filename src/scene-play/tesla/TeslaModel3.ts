34c9e70f4240297fb295e98fa53bc46393319623
亨睿 <henry.lx@alibaba-inc.com>
2021-08-31 14:51:50 +0800

    feat: add onProgress parameter

--- src/scene-play/tesla/TeslaModel3.ts
+++ src/scene-play/tesla/TeslaModel3.ts
@@ -5,10 +5,11 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 export class TeslaModel3 extends Object3D {
   private _root: Mesh | null = null;
 
-  async load() {
+  async load(onProgress?: (event: ProgressEvent) => void) {
     const loader = new GLTFLoader();
     const gltfDocument = await loader.loadAsync(
-      'models/tesla-model-3/scene.gltf'
+      'models/tesla-model-3/scene.gltf',
+      onProgress
     );
     const model = gltfDocument.scene.getObjectByName('Tesla_Model_3');
     if (model) {
