1b4658909e57be0c8ac0165fe512508edae7058a
亨睿 <henry.lx@alibaba-inc.com>
2021-08-31 14:52:04 +0800

    feat: add camera movement after loading

--- src/scene-play/tesla/TeslaScenePlay.ts
+++ src/scene-play/tesla/TeslaScenePlay.ts
@@ -1,3 +1,4 @@
+import { Easing } from '@tweenjs/tween.js';
 import { ScenePlay, ScenePlayOptions } from '../ScenePlay';
 import { TeslaModel3 } from './TeslaModel3';
 
@@ -7,12 +8,21 @@ export class TeslaScenePlay extends ScenePlay {
   private _model: TeslaModel3;
 
   constructor(canvas: HTMLCanvasElement, options: TeslaScenePlayOptions) {
-    super(canvas, { ...options });
+    super(canvas, {
+      camera: {
+        position: [8, 8, 8],
+        easing: Easing.Cubic.InOut,
+      },
+      ...options,
+    });
     this._model = new TeslaModel3();
   }
 
   async setupScene() {
     this.scene.add(this._model);
     await this._model.load();
+    this.moveCamera([
+      -5.8356834892220375, 4.567450211769818, -3.4891564032644835,
+    ], 6000);
   }
 }
