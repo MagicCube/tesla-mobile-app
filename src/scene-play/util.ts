c643f547d5e402e0035bd2f50a88d5ece6c305d2
亨睿 <henry.lx@alibaba-inc.com>
2021-08-31 17:25:00 +0800

    feat: add traverse

--- src/scene-play/util.ts
+++ src/scene-play/util.ts
@@ -1,4 +1,4 @@
-import { Vector3, Vector3Tuple } from 'three';
+import { Euler, Object3D, Vector3, Vector3Tuple } from 'three';
 
 export function vector3(point: Vector3 | Vector3Tuple): Vector3 {
   if (Array.isArray(point) && point.length === 3) {
@@ -7,10 +7,28 @@ export function vector3(point: Vector3 | Vector3Tuple): Vector3 {
   return point;
 }
 
-export function vectorToJSON(vector: Vector3 | Vector3Tuple) {
+export function vectorToJSON(vector: Vector3 | Vector3Tuple | Euler): {
+  x: number;
+  y: number;
+  z: number;
+} {
   if (Array.isArray(vector) && vector.length === 3) {
     return { x: vector[0], y: vector[1], z: vector[2] };
   } else {
     return { x: vector.x, y: vector.y, z: vector.z };
+  }
+}
+
+export function traverse(
+  obj: Object3D,
+  maxLevels: number,
+  callback: (child: Object3D, level: number) => void,
+  level = 0
+) {
+  callback(obj, level);
+  if (level < maxLevels) {
+    obj.children.forEach((child) => {
+      traverse(child, maxLevels, callback, level + 1);
+    });
   }
 }
